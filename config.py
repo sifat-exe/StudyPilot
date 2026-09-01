b2fd04fa881fae40c702d373884100210b6018b9
Ryder <sifat.ruet138@gmail.com>
2026-09-01 17:52:46 +0600

    Configure real database authentication

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 
 # Set to True to use dummy data without requiring a real database connection.
 # Set to False when the real database is ready to be integrated.
-USE_DUMMY_DATA = True
+USE_DUMMY_DATA = False
 
