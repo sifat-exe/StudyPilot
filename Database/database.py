a7efce8dc925b5eac16bea35653e3e1014bf208d
Ryder <sifat.ruet138@gmail.com>
2026-09-06 01:45:56 +0600

    checking

--- Database/database.py
+++ Database/database.py
@@ -43,7 +43,8 @@ def create_tables():
         CREATE TABLE IF NOT EXISTS users (
             user_id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            email TEXT UNIQUE NOT NULL
+            email TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL
         )
     """)
 
